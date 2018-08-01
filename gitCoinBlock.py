8dcfcb6cce8a47e110d224aad619aa18a4aaf35c
Gowtham V <gowthamv@icloud.com>
2018-08-01 22:26:09 +0530

    Fixing errors

--- gitCoinBlock.py
+++ gitCoinBlock.py
@@ -11,23 +11,31 @@ class Block: # Defining what a GitCoin consists of
     
     def hashBlock(self):
         sha = hasher.sha256() # sha256 is a hashing algorithm
-        sha.update(str(self.index)+str(self.timeStamp)+str(self.data)+str(self.previousHash))
-        retrun sha.hexdigest() # returns the combined hash value in Hexadecimals
+        sha.update(str(self.index).encode('utf-8') + str(self.timeStamp).encode('utf-8') + str(self.data).encode('utf-8') + str(self.previousHash).encode('utf-8'))
+        return sha.hexdigest() # returns the combined hash value in Hexadecimals
 
 def createGenesisBlock(): # Pre-Defining a block with index 0 and an arbitrary previous hash 
     return Block(0, date.datetime.now, "Genesis Block", 0)
 
 def createNextBlock(prevBlock): # Creating all the later blocks in the blockchain
-    currIndex = prevBlock + 1
+    currIndex = prevBlock.index + 1
     currTimeStamp = date.datetime.now()
     currData = "Block" + str(currIndex)
     currHash = prevBlock.hash
     return Block(currIndex, currTimeStamp, currData, currHash)
 
-BlockChain = [createGenesisBlock()] # Creating the BlockChain
-prevBlock = BlockChain[0] # Adding the Genesis block
+blockChain = [createGenesisBlock()] # Creating the BlockChain
+prevBlock = blockChain[0] # Adding the Genesis block
 
 blocksToCreate = 20 # No. of blocks to be added after the Genesis Block
+
+for i in range(0,blocksToCreate):
+    blockToAdd = createNextBlock(prevBlock)
+    blockChain.append(blockToAdd)
+    prevBlock = blockToAdd
+    print("Block %s has been added to the blockchain!" % (blockToAdd.index))
+    print("Hash: %s\n" % (blockToAdd.hash))  
+
 
 
 
